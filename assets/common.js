034660a197269049f5d617e64e329dfaf766d8e2
Elod Csirmaz <code@epcsirmaz.co.uk>
2026-03-23 02:30:16 +0000

    New words

--- assets/common.js
+++ assets/common.js
@@ -1,5 +1,5 @@
 
-        const bee_app_version = 231;
+        const bee_app_version = 232;
         
         if(typeof(bee_local) !== 'undefined' && bee_local.check_version) { bee_local.check_version(); }
 
