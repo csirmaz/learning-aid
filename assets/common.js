a9d557c1aa3b7f4530145830a37fdd37d43bfad2
Elod Csirmaz <code@epcsirmaz.co.uk>
2026-03-24 00:58:00 +0000

    Reschedule difficult words

--- assets/common.js
+++ assets/common.js
@@ -1,5 +1,5 @@
 
-        const bee_app_version = 232;
+        const bee_app_version = 234;
         
         if(typeof(bee_local) !== 'undefined' && bee_local.check_version) { bee_local.check_version(); }
 
