85f6b0752e7c3240f030fee3ec0ee09b983c6315
Elod Csirmaz <code@epcsirmaz.co.uk>
2026-03-24 18:55:07 +0000

    Better keyboard

--- assets/common.js
+++ assets/common.js
@@ -1,5 +1,5 @@
 
-        const bee_app_version = 237;
+        const bee_app_version = 238;
         
         if(typeof(bee_local) !== 'undefined' && bee_local.check_version) { bee_local.check_version(); }
 
