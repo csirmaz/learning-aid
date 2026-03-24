e16309097676b2ded2fb84623a33d87e854b8917
Elod Csirmaz <code@epcsirmaz.co.uk>
2026-03-24 01:48:02 +0000

    Highlight what should be typed

--- assets/common.js
+++ assets/common.js
@@ -1,5 +1,5 @@
 
-        const bee_app_version = 235;
+        const bee_app_version = 236;
         
         if(typeof(bee_local) !== 'undefined' && bee_local.check_version) { bee_local.check_version(); }
 
