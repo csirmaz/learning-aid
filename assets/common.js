de64a95c11f18f2364f66ed798b30db392eb2e39
Elod Csirmaz <code@epcsirmaz.co.uk>
2026-03-25 22:50:21 +0000

    Render images

--- assets/common.js
+++ assets/common.js
@@ -1,5 +1,5 @@
 
-        const bee_app_version = 238;
+        const bee_app_version = 245;
         
         if(typeof(bee_local) !== 'undefined' && bee_local.check_version) { bee_local.check_version(); }
 
